dbcc26e0aa5f7f0e30b22593ce0582435c4916b3
toysking <tksok2009@gmail.com>
2015-10-26 11:04:56 +0900

    import sys

--- cli-array.py
+++ cli-array.py
@@ -1,8 +1,16 @@
+#sys is get array for command line option array. 
 import sys
 
 sysargv=sys.argv
 sysargvs=len(sysargv)
 
-print sysargv[1]
-print sysargvs
+#print sysargvs
 
+if sysargvs > 1:
+ print sysargv[1]
+ print sysargvs
+
+if sysargvs==0:
+ print"please command line option"
+
+print"END"

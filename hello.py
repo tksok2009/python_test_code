fadf72b5bf443e2c15223449a3f35560ee56718b
tksok2009 <tksok2009@gmail.com>
2015-10-28 12:51:00 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,9 @@
-<<<<<<< HEAD
 word=('test2')
-=======
-word="cent os test"
->>>>>>> master
 for w in word:
  print(w)
 
 print('end')
 
-#this code is type of python2
+#python2 code example
+# word='test'
+# print 'end'

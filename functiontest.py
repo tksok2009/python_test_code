4515c018f42cc878fe5ce50b2f6bbe41238a590b
tksok2009 <tksok2009@gmail.com>
2015-10-28 18:41:26 +0900

    function and object test

---
+++ functiontest.py
@@ -0,0 +1,6 @@
+#python 3 code
+
+def return_number(num1):
+ print(num1)
+
+return_number(5)
---
+++ objecttest.py
@@ -0,0 +1,13 @@
+#python 3 code
+
+class Mynumber:
+
+ def return_number(self):
+  print(self.num1)
+
+
+if __name__ == "__main__":
+ mynum = Mynumber()
+
+ mynum.num1=5
+ mynum.return_number()

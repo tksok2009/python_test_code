878519e2f3a54a1b6a9adee2cf4c0bcdcebf89e2
admin <admin@admin-no-MacBook-Pro.local>
2015-10-31 07:23:29 +0900

    gethtml03.py add cli argv

---
+++ gethtml03.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import urllib.request, urllib.error
+import sys
+
+
+if __name__ == "__main__":
+
+ usersys=sys.argv
+ usersyslen= len(usersys)
+
+
+ if usersyslen > 1:
+  url=usersys[1]
+ else:
+  url="http://www.python-izm.com"
+
+
+ opener = urllib.request.build_opener()
+ opener.addheaders = [('User-agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; ja; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 ( .NET CLR 3.5.30729)')]
+
+ htmldata = opener.open(url)
+ print(str(htmldata.read(),"utf-8"))
+
+ htmldata.close()
+ opener.close()
